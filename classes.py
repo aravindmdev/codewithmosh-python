3a92c64e1ca90c960b7fb5eca3817a3ffa3db3c6
Aravind Mohan <aravindmohan88@hotmail.com>
2021-09-17 15:22:08 -0400

    updates for 09-16

--- classes.py
+++ classes.py
@@ -55,9 +55,51 @@ class Point4:
     def __str__(self):  # Invoked when objects are converted to string
         return (f"({self.x},{self.y})")
 
+    # Override and implement the equality according to us.
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
 
 point4 = Point4(1, 2)
 # This will print (1,2). If __str__ was not overriden then it would print the memory address.
 print(point4)
 point41 = Point4(1, 2)
 print(point4 == point41)  # equality will invoke the __eq__ magic method
+
+
+# Implementing a class with different functionalities
+# Private attributes. Prefix with __
+# Technically we still can access private attributes. Get the hidden name from <object>.__dict__
+# Python does not complete private setting. Its not about security. Its just warning.
+
+class TagCloud:
+    def __init__(self):
+        self.__tags = {}  # Initializes a dictionary
+
+    def add(self, tag):
+        # Checks if entry is there, if not 0, then add 1 to that.
+        # .lower is used to avoid case sensistivity
+        self.__tags[tag.lower()] = self.__tags.get(tag.lower(), 0) + 1
+
+    def __getitem__(self, tag):
+        return self.__tags.get(tag.lower(), 0)  # returns count of tag
+
+    def __setitem__(self, tag, count):
+        self.__tags[tag.lower()] = count
+
+    def __len__(self):
+        return len(self.__tags)
+
+    def __iter__(self):
+        return iter(self.__tags)
+
+
+cloud = TagCloud()  # Create the object
+cloud.add("Python")
+cloud.add("python")
+cloud.add("PYthon")
+cloud["python"]  # this invokes __getitem__ magic method
+cloud["python"] = 10  # invokes __setitem__ magic method
+len(cloud)  # invokes __len__ magic method
+for tag in cloud:  # invokes the __iter__ method
+    print(tag)
