c0066316394ee6e96ee596d41cecd9f5dc053038
Aravind Mohan <aravindmohan88@hotmail.com>
2021-09-20 09:16:49 -0400

    updates for 09-19-21

---
+++ Kforce_Test.py
@@ -0,0 +1,31 @@
+n = 123456
+sn = str(n)
+p = 1
+s = 0
+for d in sn:
+    p *= int(d)
+    s += int(d)
+
+print(p-s)
+
+pattern = "0"
+s = "y"
+
+patlen = len(pattern)
+slen = len(s)
+limit = slen - patlen
+matchcnt = 0
+for i in range(0, limit + 1):
+    sSub = s[i:(i + patlen)]
+    patmatch = []
+    for j in range(0, patlen):
+        if sSub[j].lower() in ('a', 'e', 'i', 'o', 'u') and pattern[j] == "0":
+            patmatch.append("T")
+        elif sSub[j].lower() not in ('a', 'e', 'i', 'o', 'u') and pattern[j] == "1":
+            patmatch.append("T")
+        else:
+            patmatch.append("F")
+    if "F" not in patmatch:
+        matchcnt += 1
+
+print(matchcnt)
--- classes.py
+++ classes.py
@@ -1,4 +1,8 @@
 # Declaration
+from collections import namedtuple
+from abc import ABC, abstractmethod
+
+
 class Point:
     def draw(self):  # functions should have atleast one argument. Self refers to the current object
         print("draw")
@@ -10,7 +14,7 @@ print(isinstance(point, Point))
 
 
 class Point2:
-    default_color = "red"  # class level attribute. will be shred across all instances
+    default_color = "red"  # class level attribute. will be shared across all instances
 
     def __init__(self, x, y):  # Constructor - will be called when the object is created. Self will be automatically passed by Python
         self.x = x  # instance level attributes. Ca be different for each object
@@ -103,3 +107,167 @@ cloud["python"] = 10  # invokes __setitem__ magic method
 len(cloud)  # invokes __len__ magic method
 for tag in cloud:  # invokes the __iter__ method
     print(tag)
+
+# Get Set Properties
+
+# This implementation is not pythonic
+
+
+class Product1:
+    def __init__(self, value):
+        self.set_price(value)
+
+    def get_price(self):
+        return self.__price
+
+    def set_price(self, value):
+        if value < 0:
+            raise ValueError("Price cannot be negative")
+        self.__price = value
+
+
+product1 = Product1(-10)
+product1.set_price(-20)
+product1.get_price()
+
+# Pythonic implementation of above code
+
+
+class Product2:
+    def __init__(self, value):
+        self.price = value
+
+    @property
+    def price(self):  # the attribute name will be the function name
+        return self.__price
+
+    @price.setter
+    def price(self, value):
+        if value < 0:
+            raise ValueError("Price cannot be negative")
+        self.__price = value
+
+
+product2 = Product2(-10)
+product2.price = -20  # now the price can be accessed as an attribute
+
+# Inheritance
+
+
+class Animal:
+    def __init__(self):
+        self.age = 1
+
+    def eat(self):
+        print("eat")
+
+
+# Inherits Animal class. to inlcude multiple separate with commas.
+# During multiple inheritance if both the class have the same methods then the method from the class given first will be executed.
+class Mammal(Animal):
+    # to call the contructor of the parent class. This is not needed if the child class does not have constructor.
+    super.__init__()
+
+    def __init__(self):
+        self.weight = 2  # this will override the Animal constructor.
+
+    def walk(self):
+        print("walk")
+
+
+class Fish(Animal):
+    def swim(self):
+        print("swim")
+
+
+m = Mammal()
+f = Fish()
+
+print(m.age)  # will print 1
+m.eat()
+f.eat()
+
+# Some in-built functions
+print(isinstance(m, Mammal))  # will be true
+print(isinstance(m, object))  # all classes inherit from object class
+# checks whether the class inherits from another
+print(issubclass(Mammal, Animal))
+
+
+# Proper inheritance, Abstract class and custom exception
+# To create abstract class and methods define this "from abc import ABC, abstractmethod"
+# Custom exception
+
+
+class InvalidOperationError(Exception):
+    pass
+
+
+class Stream(ABC):  # ABC - Abstract base class. To create the abstract method
+    def __init__(self):
+        self.opened = False
+
+    def open(self):
+        if self.opened:
+            raise InvalidOperationError("Stream is already opened")
+        self.opened = True
+
+    def close(self):
+        if not self.opened:
+            raise InvalidOperationError("Stream is already closed")
+        self.opened = False
+
+    @abstractmethod  # decorator for the abstract method
+    def read(self):
+        # No implemenation. Whichever class inherits the stream class needs to implement this.
+        # Otherwise the object cannot be created for the child class.
+        pass
+
+
+class FileStream(Stream):
+    def read(self):
+        print("Reading data from the file")
+
+
+class NetworkStream(Stream):
+    def read(self):
+        print("Reading data from the network")
+
+# Polymorphism
+
+
+class UIControl(ABC):
+    @abstractmethod
+    def draw(self):
+        pass
+
+
+class TextBox(UIControl):
+    def draw(self):
+        print("TextBox")
+
+
+class DropDown(UIControl):
+    def draw(self):
+        print("DropDown")
+
+
+def draw(control):  # this method which all controls are going to be executed. Mnay forms - Polymorphism
+    control.draw()
+
+
+TB = TextBox()
+DD = DropDown()
+
+# Even if the classes dont inherit UIControl it works. The draw function just passes the object and as long it is iterable it will work.
+# Achieving polymorphism without inheritance is called Duck Typing.
+draw([TB, DD])
+
+# namedtuple - from collections import namedtuple
+# can be used to repreesnt data classes
+# immutable. If we wnat chnage values recrate the object
+Point = namedtuple("Point", ["x", "y"])
+p1 = Point(x=1, y=2)
+p1 = Point(x=2, y=3)  # to change the values
+# will return true. No need to implement the magic method __eq__
+p2 = Point(x=2, y=3)
